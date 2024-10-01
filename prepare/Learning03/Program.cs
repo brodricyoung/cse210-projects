a7166277876de6bd85c5171d3d26c1d79e50e8b0
brodricyoung <you22030@byui.edu>
2024-10-01 12:07:56 -0600

    Completed Learning03 activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(6);
+        Fraction f3 = new Fraction(6, 7);
+
+        // looked at changing variables in debugger
+        int f3Top = f3.GetTop();
+        int f3Bottom = f3.GetBottom();
+        f3.SetTop(3);
+        f3.SetBottom(4);
+        int newf3Top = f3.GetTop();
+        int newf3Bottom = f3.GetBottom();
+
+        string frac = f3.GetFractionString();
+        double deci = f3.GetDecimalValue();
+
+        Console.WriteLine($"frac: {frac}, deci: {deci}");
+
+
     }
 }
